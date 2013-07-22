1893c1eca4ec61468e01b1d9ede54003da7027bc
Vincent vandeWiel <VinceZero4@gmail.com>
2013-07-22 10:52:34 +0000

    update web.js with html index

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
+var buffer = new Buffer(fs.readFileSync(index.html), "utf-8");
+
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
